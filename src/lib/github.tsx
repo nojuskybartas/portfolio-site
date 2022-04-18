b5159f0c460f383dd8a5b9d435dbf28fc37817a1
nojuskybartas <nojus.kybartas@gmail.com>
2022-04-18 22:46:45 +0200

    working codeSnap

--- src/lib/github.tsx
+++ src/lib/github.tsx
@@ -1,13 +1,28 @@
-import { NextRouter, useRouter } from "next/router"
-import { Children, cloneElement, ReactElement, ReactNode } from "react"
+import axios from "axios"
+import { NextRouter } from "next/router"
 
-
-export const getCodeData = async(router:NextRouter) => {
+export const getCodeData = async(router: NextRouter) => {
     const file = router.pathname === '/' ? '/index' : router.pathname
-    const location = '/pages' + file
-    console.log(location)
-    const res = await fetch('https://raw.githubusercontent.com/nojuskybartas/cv/main' + location)
+    const url = `https://raw.githubusercontent.com/nojuskybartas/portfolio-site/main/pages${file}.tsx`
+    const res = await fetch(url)
     const codeData = await res.text()
-    
     return codeData
+}
+
+export const getCodeImage = async(code: string) => {
+    const response = await fetch('https://sourcecodeshots.com/api/image', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            code: code,
+            settings: {
+                language: 'js',
+                theme: 'dark-plus',
+            },
+        }),
+    });
+
+    const blob = await response.blob()
+
+    return URL.createObjectURL(blob);
 }
